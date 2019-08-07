654304457364c244014b2216be7d198ab4104a39
tcdian <tcdiank@gmail.com>
2019-08-07 23:22:05 +0800

    :truck: add demo

---
+++ src/components/demo/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+const Demo: React.FC = () => {
+    return (
+        <div className="test">
+            testT
+        </div>
+    );
+}
+
+export default Demo;
